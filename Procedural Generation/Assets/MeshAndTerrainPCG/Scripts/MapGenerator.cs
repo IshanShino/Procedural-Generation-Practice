86c9225a4d3e5e79c15c63d6f18552843dc20d91
IshanShino <ishans1045@gmail.com>
2024-05-28 19:00:35 +0530

    Octaves
    Octaves, Persistence, Lacunarity, Noise Height
    Generated multiple random maps using a seed value and randomizing that

--- Procedural Generation/Assets/MeshAndTerrainPCG/Scripts/MapGenerator.cs
+++ Procedural Generation/Assets/MeshAndTerrainPCG/Scripts/MapGenerator.cs
@@ -1,16 +1,48 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public int width;
-    public int height;
+    public int mapWidth;
+    public int mapHeight;
     public float noiseScale;
+
+    public int seed;
+    public int octaves;
+    [Range(0, 1)] public float persistence;
+    public float lacunarity;
+    public Vector2 offset;
+
     public bool autoUpdate;
     public void GenerateMap()
     {
-        float[,] noiseMap =  Noise.GenerateNoiseMap(width, height, noiseScale);
+        float[,] noiseMap =  Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, seed, octaves, persistence, lacunarity, offset);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate() // called automatically whenever the script values are changes in the inspector
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (noiseScale < 0.3f)
+        {
+            noiseScale = 0.3f;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
     }
 }
--- Procedural Generation/Assets/MeshAndTerrainPCG/Scripts/Noise.cs
+++ Procedural Generation/Assets/MeshAndTerrainPCG/Scripts/Noise.cs
@@ -1,29 +1,70 @@
 using UnityEngine;
-
 public static class Noise
 {
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int seed, int octaves, float persistence, 
+    float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        System.Random prng = new System.Random(seed); // generate randomized maps on different seeds
+        Vector2[] octaveOffsets = new Vector2[octaves];
+
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
 
         if (scale <= 0)
         {
             scale = 0.0001f;
         }
 
+        float minNoiseHeight = float.MaxValue;
+        float maxNoiseHeight = float.MinValue;
+
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
         for (int z = 0; z < mapHeight; z++)
         {
             for (int x = 0 ; x < mapWidth; x++)
             {   
-                  
-                float sampleX = x / scale;
-                float sampleY = z / scale;
+                float amplitude = 1f;
+                float frequency = 1f;
+                float noiseHeight = 0f;
 
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap[x, z] = perlinValue;
-                
+                for (int i = 0; i < octaves; i++)
+                {   
+                    float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
+                    float sampleZ = (z - halfHeight) / scale * frequency + octaveOffsets[i].y;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleZ) * 2 - 1;  // * 2 - 1 is done so that the perlin noise can sometimes be negative  
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistence;  // persistence < 1. amplitude deacreases each octave 
+                    frequency *= lacunarity;  //  lacunarity < 1. amplitude deacreases each octave 
+                }    
+
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }  
+                else if (noiseHeight < minNoiseHeight)    
+                {
+                    minNoiseHeight = noiseHeight;
+                }    
+                noiseMap[x, z] = noiseHeight;
             }
         }
+         for (int z = 0; z < mapHeight; z++)
+        {
+            for (int x = 0 ; x < mapWidth; x++)
+            {
+                noiseMap[x, z] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, z]); // nomralizing the noiseMap so that all the values are between 0&1.
+            }
+        }  
         return noiseMap;
     }
 }
